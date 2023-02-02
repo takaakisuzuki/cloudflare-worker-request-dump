25f947f7d531a83431f0cc65ea288bda990df549
Takaaki Suzuki (captain) <taka@cloudflare.com>
2023-02-02 13:55:05 +0900

    Update index.ts
    Update kv

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 export default {
-  async fetch(request) {
+  async fetch(request, env) {
     let html_content = '';
     let html_style = 'body{padding:6em; font-family: sans-serif;} h1{color:#f6821f;} div.check {padding: 0px 0px 0px 0px; display: table; margin: 36px auto auto auto;}';
-    let value = await MY_KV.get("cloudflare");
+    let value = await env.MY_KV.get("cloudflare");
     const html_uuid = crypto.randomUUID();
         
     html_content += '<p> <strong> Worker KV: </strong> ' + value + '</p>';
