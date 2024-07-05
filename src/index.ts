6a5850899d8995bb44c49ea2bb391041252dfddd
Takaaki Suzuki (captain) <taka@cloudflare.com>
2024-07-05 14:19:47 +0900

    Update index.ts
    updated

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,9 @@ export default {
     let html_content = '';
     let html_style = 'body{padding:6em; font-family: sans-serif;} h1{color:#f6821f;} div.check {padding: 0px 0px 0px 0px; display: table; margin: 36px auto auto auto;}';
     //const html_uuid = crypto.randomUUID();
-    const { searchParams } = new URL(request.url)
-    let name = searchParams.get('uuid')    
-    let value = await env.MY_KV.get('name');
+    const url = new URL(request.url);
+    const myparam = url.searchParams.get('uuid');    
+    let value = await env.MY_KV.get('myparam');
     
     //await env.MY_BUCKET.put(html_uuid, 'Test write R2 Bucket');
     await env.MY_Q.send({
